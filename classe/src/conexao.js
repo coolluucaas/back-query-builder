6f37cb8e5cd5a55ebb07b0d90b331a47a55d1dd7
coolluucaas <lucaspsanta@gmail.com>
2021-08-26 14:54:26 -0300

    knexjs configurado

--- classe/src/conexao.js
+++ classe/src/conexao.js
@@ -1,17 +1,28 @@
-const { Pool } = require('pg');
+const { Pool } = require('pg')
 
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'market_cubos',
     password: 'postgres',
-    port: 5432
-});
+    port: 5432,
+})
+
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: 'localhost',
+        user: 'postgres',
+        password: 'cool$enh@',
+        database: 'market_cubos_knexjs',
+    },
+})
 
 const query = (text, param) => {
-    return pool.query(text, param);
+    return pool.query(text, param)
 }
 
 module.exports = {
-    query
+    query,
+    knex,
 }
